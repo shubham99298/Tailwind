36098beb16e38aa0b882b496a49eb0670763a638
Shubham <shubhamlata2002@gmail.com>
2023-09-20 22:12:23 +0530

    tailwind

--- src/App.jsx
+++ src/App.jsx
@@ -8,7 +8,7 @@ function App() {
 
   return (
     <>
-     <div className="bg-amber-50 min-h-screen px-6 py-3 flex flex-col gap-16 ">
+     <div className="bg-amber-50 min-h-screen px-6 py-3 flex flex-col gap-15 ">
       <Header/>
       <Body/>
       <Footer/>
